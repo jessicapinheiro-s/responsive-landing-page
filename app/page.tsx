106701ce7ef44515af5301ec48fb1ff1960cded5
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2025-05-25 15:28:15 -0300

    Ajustes no espaçamento dos elementos da página conforme especificado

--- app/page.tsx
+++ app/page.tsx
@@ -18,80 +18,87 @@ export default function Home() {
   return (
     <React.Fragment>
       <Header />
-      <main>
+      <main className="flex flex-col gap-y-[28px]">
         <div>
           <Image
             src={banner}
             alt="banner"
+            className="w-full h-auto"
           />
         </div>
-        <div>
-          <div>
-            <h1>Our Clients</h1>
-            <p>We have been working with some Fortune 500+ clients</p>
+        <div className="w-full px-[100px] flex flex-col justify-center items-center gap-[11px]">
+          <div className="w-[772px] text-center">
+            <h1 className="text-[25px] font-semibold">Our Clients</h1>
+            <p className="text-[11px]">We have been working with some Fortune 500+ clients</p>
           </div>
-          <div className="flex flex-row justify-between items-center">
+          <div className="w-[800px] flex flex-row justify-between items-center gap-10">
             <Image
               src={icon1}
               alt="icon1"
-            />
-            <Image
-              src={icon2}
-              alt="icon2"
+              width={33}
             />
             <Image
               src={icon3}
               alt="icon3"
+              width={33}
             />
             <Image
               src={icon4}
               alt="icon4"
+              width={33}
             />
             <Image
               src={icon5}
               alt="icon5"
+              width={33}
             />
             <Image
               src={icon6}
               alt="icon6"
+              width={33}
             />
             <Image
               src={icon7}
               alt="icon7"
+              width={33}
             />
             <Image
               src={icon8}
               alt="icon8"
+              width={33}
             />
           </div>
         </div>
-        <div>
-          <div>
-            <h1>Manage your entire community in a single system</h1>
-            <p>Who is Nextcent suitable for?</p>
+        <div className="w-full">
+          <div className="text-center">
+            <h1 className="text-[25px] font-semibold">Manage your entire community in a single system</h1>
+            <p className="text-[11px]">Who is Nextcent suitable for?</p>
           </div>
-          <div className="flex flex-row justify-between items-center">
-            <div>
-              <div>
+          <div className="w-full">
+            <div className="w-full flex flex-row justify-center items-center">
+              <div className="w-[208px] h-[182px] break-words text-center flex flex-col justify-center items-center gap-[5px] border px-[22px] py-[16px]">
                 <Image
                   src={icon11}
                   alt="icon11"
                 />
-                <h2>Membership Organisations</h2>
+                <h2 className="text-[19px] font-bold">Membership Organisations</h2>
+                <p>Our membership management software provides full automation of membership renewals and payments</p>
               </div>
-              <div>
+              <div className="w-[208px] h-[182px] break-words text-center flex flex-col justify-center items-center gap-[5px] border px-[22px] py-[16px]">
                 <Image
                   src={icon12}
                   alt="icon12"
                 />
-                <h2>National Associations</h2>
+                <h2 className="text-[19px] font-bold">National Associations</h2>
+                <p>Our membership management software provides full automation of membership renewals and payments</p>
               </div>
-              <div>
+              <div className="w-[208px] h-[182px] break-words text-center flex flex-col justify-center items-center gap-[5px] border px-[22px] py-[16px]">
                 <Image
                   src={icon13}
                   alt="icon13"
                 />
-                <h2>Clubs And Groups</h2>
+                <h2 className="text-[19px] font-bold">Clubs And Groups</h2>
+                <p>Our membership management software provides full automation of membership renewals and payments</p>
               </div>
             </div>
           </div>
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -5,24 +5,27 @@ import Image from "next/image";
 
 export function Header() {
     return (
-        <header className="bg-white w-full flex flex-row items-center justify-between px-[100px] py-[11px] gap-[43px]">
-            <div>
+        <header className="h-[58px] bg-white w-full flex flex-row items-center justify-between px-[100px] py-[11px] gap-[43px]">
+            <div className="w-2/12">
                 <Image
                     alt="Logo"
                     src={logo}
+                    width={100}
                 />
             </div>
-            <div>
-                <nav className="flex flex-row justify-center items-center">
-                    <ul>Home</ul>
-                    <ul>Features</ul>
-                    <ul>Community</ul>
-                    <ul>Blog</ul>
-                    <ul>Pricing</ul>
-                </nav>
-            </div>
-            <div>
-                <button className="bg-primary text-white">Registrer Now</button>
+            <div className="w-8/12 flex flex-row items-center justify-end gap-[22px]">
+                <div className="w-full ">
+                    <nav className="flex flex-row justify-between items-center">
+                        <ul>Home</ul>
+                        <ul>Features</ul>
+                        <ul>Community</ul>
+                        <ul>Blog</ul>
+                        <ul>Pricing</ul>
+                    </nav>
+                </div>
+                <div>
+                    <button className="bg-primary text-white px-[22px] py-[10px]">Registrer Now</button>
+                </div>
             </div>
         </header>
 
