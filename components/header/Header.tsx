13bc38e47f2440ab060e057007da704c51346549
Jéssica Pinheiro <jessicasilva.js1314@gmail.com.br>
2025-07-09 22:55:19 -0300

    Ajustando espaçamento do header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -5,16 +5,16 @@ import Image from "next/image";
 
 export function Header() {
     return (
-        <header className="h-[58px] bg-white w-full flex flex-row items-center justify-between px-[100px] py-[11px] gap-[43px]">
-            <div className="w-2/12">
+        <header className="bg-white w-full flex flex-row items-center justify-between px-[100px] py-[12px] gap-[43px] ">
+            <div className="w-2/12 ">
                 <Image
                     alt="Logo"
                     src={logo}
                     width={100}
                 />
             </div>
-            <div className="w-8/12 flex flex-row items-center justify-end gap-[22px]">
-                <div className="w-full ">
+            <div className=" w-10/12 flex flex-row items-center justify-end gap-[22px]">
+                <div className="w-6/12">
                     <nav className="flex flex-row justify-between items-center">
                         <ul>Home</ul>
                         <ul>Features</ul>
@@ -23,8 +23,8 @@ export function Header() {
                         <ul>Pricing</ul>
                     </nav>
                 </div>
-                <div>
-                    <button className="bg-primary text-white px-[22px] py-[10px]">Registrer Now</button>
+                <div >
+                    <button className="bg-primary text-white px-[22px] py-[10px] rounded-lg">Registrer Now</button>
                 </div>
             </div>
         </header>
